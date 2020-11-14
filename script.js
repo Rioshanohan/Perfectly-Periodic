f87e36f04e6e75a65550f2a99fe41712d5a2a825
Rioshanohan <46582183+Rioshanohan@users.noreply.github.com>
2020-11-13 19:58:18 -0500

    fri nov 13

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function doOnLoad () {
+  getinput(1)
+  elemsInit()
+  var cnv = document.getElementById('myCanvas')
+  var ctx = cnv.getContext('2d')
+  ctx.fillStyle = '#FF0000'
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI)
+  ctx.stroke()
+}
+
 const elements = {}
 var a = 'Hydrogen Helium Lithium Beryllium Boron Carbon Nitrogen Oxygen Fluorine Neon Sodium Magnesium Aluminum Silicon Phosphorus Sulfur Chlorine Argon Potassium Calcium Scandium Titanium Vanadium Chromium Manganese Iron Cobalt Nickel Copper Zinc Gallium Germanium Arsenic Selenium Bromine Krypton Rubidium Strontium Yttrium Zirconium Niobium Molybdenum Technetium Ruthenium Rhodium Palladium Silver Cadmium Indium Tin Antimony Tellurium Iodine Xenon Cesium Barium Lanthanum Cerium Praseodymium Neodymium Promethium Samarium Europium Gadolinium Terbium Dysprosium Holmium Erbium Thulium Ytterbium Lutetium Hafnium Tantalum Tungsten Rhenium Osmium Iridium Platinum Gold Mercury Thallium Lead Bismuth Polonium Astatine Radon Francium Radium Actinium Thorium Protactinium Uranium Neptunium Plutonium Americium Curium Berkelium Californium Einsteinium Fermium Mendelevium Nobelium Lawrencium Rutherfordium Dubnium Seaborgium Bohrium Hassium Meitnerium Darmstadtium Roentgenium Copernicum Nihonium Flerovium Moscovium Livermorium Tennessine Oganesson'
 var b = 'H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og'
@@ -93,10 +103,17 @@ function setCurrentById (newElem) {
   newElemPush.push('current')
   document.getElementById(newElem).className = newElemPush.join(' ')
 }
-function boom () {
-  var baboom = document.getElementsByClassName('current')
-  for (var ii = 0; ii < baboom.length; ii++) {
-    console.log(baboom[ii].className)
+function boom (param) {
+  switch (param) {
+    case 'jeff':
+      console.log('jeff')
+      break
+    case 'bob':
+      console.log('bob')
+      break
+    default:
+      console.log('you got noped')
+      break
   }
 }
 function generate (index) {
