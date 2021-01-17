5f38e377b23293b0b6d1834675b1162595cfd010
Rioshanohan <rohanatl07@gmail.com>
2021-01-17 17:04:43 -0500

    test

--- test/script.js
+++ test/script.js
@@ -2,4 +2,5 @@ fetch('sample.json')
     .then(response => response.json())
     .then(data => {
         console.log(data)
+        document.getElementById('main-paragraph').innerHTML = data.sentence
     })
